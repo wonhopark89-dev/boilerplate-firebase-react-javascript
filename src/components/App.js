cac01e3821c6d631e828965a89ebd3e0ce3df4ec
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-10 02:32:59 +0900

    edit jsonconfig

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+// import fbase from 'fbase';
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_API_KEY,
@@ -11,4 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
