771eda4ee87f830a030b41100ce908c98d9b6ae3
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-11 01:41:28 +0900

    add firebase eamil auth

--- src/fbase.js
+++ src/fbase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { authService } from 'fbase';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (event) => {
     // console.log(event.target.name);
@@ -17,16 +19,28 @@ const Auth = () => {
     }
   };
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault(); // don`t want to do default function
+    try {
+      if (newAccount) {
+        // create account
+        // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createuserwithemailandpassword
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        // login
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={onsubmit}>
-        <input name="email" type="text" placeholder="email" required value={email} onChange={onChange} />
+      <form onSubmit={onSubmit}>
+        <input name="email" type="email" placeholder="email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
       </form>
       <div>
         <button>Continue with Google</button>
