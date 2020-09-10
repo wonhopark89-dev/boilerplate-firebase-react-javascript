6efd39b0b979bb957cd21cfe2552a13617a0fd10
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-11 00:29:06 +0900

    add login option

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (event) => {
+    // console.log(event.target.name);
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onsubmit}>
+        <input name="email" type="text" placeholder="email" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
