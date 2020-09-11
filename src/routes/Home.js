139b35cb599c8e3c23b52faa4f2d6caf2df6d185
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-11 17:19:31 +0900

    setup firebase database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What`s on your mind?"
+          maxLength={120}
+        ></input>
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
