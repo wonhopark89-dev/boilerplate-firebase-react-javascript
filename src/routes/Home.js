a08903e338a0f37c1d31edbd71b383ba96033e62
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-12 03:12:09 +0900

    add preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Tweet from 'components/Tweet';
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState('');
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection('tweets').onSnapshot((snapshot) => {
@@ -42,11 +43,18 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     // https://developer.mozilla.org/ko/docs/Web/API/FileReader
     const reader = new FileReader();
-    reader.onloadstart = (finishEvent) => {
-      console.log(finishEvent);
+    // 이미지 가져오기
+    reader.onloadend = (finishEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishEvent;
+
+      setAttachment(result);
     };
     reader.readAsDataURL(theFile);
   };
+
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -54,6 +62,12 @@ const Home = ({ userObj }) => {
         <input value={tweet} onChange={onChange} type="text" placeholder="What`s on your mind?" maxLength={120}></input>
         <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="tweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {tweets.map((tomato) => (
