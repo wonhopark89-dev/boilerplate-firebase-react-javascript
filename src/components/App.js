9a6b78bc1a416a92112b0fe31bc5c7ade8801157
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-12 00:31:00 +0900

    improve status management with userId

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import { authService } from 'fbase';
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null); // 로그인 시 유저정보를 넣어준다
 
   useEffect(() => {
     // login, logout
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -20,7 +22,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { dbService } from 'fbase';
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState('');
   const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
-    getTweets();
-  }, []);
+    dbService.collection('tweets').onSnapshot((snapshot) => {
+      const tweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-  const getTweets = async () => {
-    const dbTweets = await dbService.collection('tweets').get(); // tweets 이름의 컬렉션을 가지고옴
-    dbTweets.forEach((document) => {
-      const tweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setTweets((prev) => [tweetObject, ...prev]);
+      setTweets(tweetArray);
     });
-  };
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection('tweets').add({
-      tweet,
+      text: tweet,
       createdAt: Date.now(),
+      createdId: userObj.uid,
     });
     setTweet('');
   };
@@ -41,19 +38,13 @@ const Home = () => {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input
-          value={tweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What`s on your mind?"
-          maxLength={120}
-        ></input>
+        <input value={tweet} onChange={onChange} type="text" placeholder="What`s on your mind?" maxLength={120}></input>
         <input type="submit" value="tweet" />
       </form>
       <div>
         {tweets.map((tomato) => (
           <div key={tomato.id}>
-            <h4>{tomato.tweet}</h4>
+            <h4>{tomato.text}</h4>
           </div>
         ))}
       </div>
