b4a77d506b827362cc30284d1be9d28498184a1e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-13 21:16:08 +0900

    add update profile, small object

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,34 @@ function App() {
     // login, logout
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUserObj(user);
+        // setUserObj(user); // user object is so big
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       } else {
       }
       setInit(true);
     });
   }, []);
 
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : 'Initializing...'}
+      {init ? (
+        <AppRouter refreshUser={refreshUser} isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        'Initializing...'
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { authService } from 'fbase';
 import { useHistory } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      await userObj.updateProfile({ displayName: newDisplayName });
+    }
+    refreshUser();
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="Display your name" value={newDisplayName} />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 };
+
 export default Profile;
